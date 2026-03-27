c90e4f7fc4b2a0f3a6de42c760ad36420749a5dd
Kanha-1234 <ashutoshnayak0001@gmail.com>
2026-03-27 08:55:11 +0530

    marshaling unmarshaling d2

--- marshaling_unmarshaling/main.go
+++ marshaling_unmarshaling/main.go
@@ -11,15 +11,57 @@ import (
 // 	City string `json:"city"`
 // }
 
-type Address struct {
-    Street string `json:"street"`
-    City   string `json:"city"`
+// type Address struct {
+// 	Street string `json:"street"`
+// 	City   string `json:"city"`
+// }
+
+// type Person struct {
+// 	Name    string  `json:"name"`
+// 	Age     int     `json:"age"`
+// 	Address Address `json:"address"` // ✅ nested struct
+// }
+
+// type MyTime struct {
+// 	time.Time
+// }
+
+// // Custom marshal — output as Unix timestamp
+// func (t MyTime) MarshalJSON() ([]byte, error) {
+// 	return json.Marshal(t.Unix())
+// }
+
+// // Custom unmarshal — read Unix timestamp back
+// func (t *MyTime) UnmarshalJSON(data []byte) error {
+// 	var unix int64
+// 	if err := json.Unmarshal(data, &unix); err != nil {
+// 		return err
+// 	}
+// 	t.Time = time.Unix(unix, 0)
+// 	return nil
+// }
+
+type Book struct {
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price"`
 }
 
-type Person struct {
-    Name    string  `json:"name"`
-    Age     int     `json:"age"`
-    Address Address `json:"address"` // ✅ nested struct
+type Shoes struct {
+	Brand   string `json:"brand"`
+	Size    int    `json:"size"`
+	InStock bool   `json:"in_stock"`
+}
+
+type Movie struct{
+	Title string `json:"movie_title"`
+	Year  string  `json:"release_year"`
+	Rating string  `json:"rating,omitempty"`
+}
+
+type Students struct{
+	Name string `json:"name"`
+	Grade string `json:"grade"`
 }
 
 func main() {
@@ -37,18 +79,18 @@ func main() {
 
 	// fmt.Println(string(jsonData))
 
-// 	person := Person{
-//     Name: "Rahul",
-//     Age:  25,
-//     Address: Address{
-//         Street: "MG Road",
-//         City:   "Hyderabad",
-//     },
-// }
+	// 	person := Person{
+	//     Name: "Rahul",
+	//     Age:  25,
+	//     Address: Address{
+	//         Street: "MG Road",
+	//         City:   "Hyderabad",
+	//     },
+	// }
 
-// jsonData, _ := json.Marshal(person)
-// fmt.Println(string(jsonData))
-// {"name":"Rahul","age":25,"address":{"street":"MG Road","city":"Hyderabad"}}
+	// jsonData, _ := json.Marshal(person)
+	// fmt.Println(string(jsonData))
+	// {"name":"Rahul","age":25,"address":{"street":"MG Road","city":"Hyderabad"}}
 
 	// jsonString := `{"name":"Rahul","age":25,"city":"Hyderabad"}`
 
@@ -65,9 +107,43 @@ func main() {
 	// fmt.Println(person.City) // Hyderabad
 	// fmt.Println(person)
 
-	jsonString := `{"name":"Rahul","age":25,"address":{"street":"MG Road","city":"Hyderabad"}}`
-var person Person
-json.Unmarshal([]byte(jsonString), &person)
-fmt.Println(person) // Hyderabad
+	// jsonString := `{"name":"Rahul","age":25,"address":{"street":"MG Road","city":"Hyderabad"}}`
+	// var person Person
+	// json.Unmarshal([]byte(jsonString), &person)
+	// fmt.Println(person) // Hyderabad
+
+	//Q1- Books := Book{Title: "book title",
+	// 	Author: "book author",
+	// 	Price:  45.89,
+	// }
+
+	// bookjson, err := json.Marshal(Books)
+	// if err == nil {
+	// 	fmt.Println("egerg")
+	// }
+	// fmt.Println(string(bookjson))
+
+	//Q2- jsonString := `{"brand":"Nike","size":42,"in_stock":true}`
+	// var shoes Shoes
+	// json.Unmarshal([]byte(jsonString), &shoes)
+	// fmt.Println(shoes)
+
+	// Q3 - movie := Movie{
+	// 	Title: "Kick",
+	// 	Year: "20/02/2002",
+	// 	Rating : "5star",
+	// }
+
+	// output,_ := json.Marshal(movie)
+	// fmt.Println(string(output))
+
+	//q4 jsonString := []Students{
+	// 	{Name:"ashutosh",Grade:"A"},
+	// 	{Name:"kanha",Grade:"B"},
+	// 	{Name:"Jhon",Grade:"c"},
+	// }
+
+	// output,_ := json.Marshal(jsonString)
+	// fmt.Println(string(output))
 
 }
